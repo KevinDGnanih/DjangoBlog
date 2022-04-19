b9f378231bb9b85a4c1d8f950d84046f827434e2
KevinDGnanih <kevin.gnanih@gmail.com>
2022-04-19 20:24:00 +0000

    fixed mistype in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2022kdg.herokyapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar2022kdg.herokuapp.com', 'localhost']
 
 
 # Application definition
