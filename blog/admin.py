5af695391fb78e40dd86b8478a809ba6bb16c9ed
KevinDGnanih <kevin.gnanih@gmail.com>
2022-04-19 23:09:00 +0000

    Added View comments and open a post

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,6 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'created_on')
@@ -17,7 +16,7 @@ class PostAdmin(SummernoteModelAdmin):
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'body', 'post', 'created_on','approved')
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ['name', 'email', 'body']
     actions = ['approve_comments']
